758aa49c6121006b0bb71fbce2a9076033bdbfda
Annah Isenberg <annahisenberg@gmail.com>
2018-05-29 21:18:50 -0400

    Add blog posts tests

--- server.js
+++ server.js
@@ -7,9 +7,45 @@ const blogPostsRouter = require('./blogPostsRouter');
 
 app.use(morgan('common'));
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'index.html');
+});
+
 app.use('/blog-posts', blogPostsRouter);
 
+let server;
 
-app.listen(process.env.PORT || 8080, () => {
-    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+function runServer() {
+    const port = process.env.PORT || 8080;
+    return new Promise((resolve, reject) => {
+        server = app.listen(port, () => {
+            console.log(`Your app is listening on port ${port}`);
+            resolve(server);
+        }).on('error', err => {
+            reject(err)
+        });
+    });
+}
+
+function closeServer() {
+    return new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+};
+
+module.exports = {
+    app,
+    runServer,
+    closeServer
+};
